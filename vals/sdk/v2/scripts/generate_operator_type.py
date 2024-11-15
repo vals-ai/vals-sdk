2f7bc790cee91695c5d7bb96e7e87ac6b7076a59
Langston Nashold <lgnashold@gmail.com>
2024-11-14 17:28:49 -0800

    Add type literal to operators (#13)
    * Add type literal to operators
    
    * Switch

---
+++ vals/sdk/v2/scripts/generate_operator_type.py
@@ -0,0 +1,28 @@
+"""
+Internal script to regenerate the operator_type.py file. 
+
+Run with python generate_operator_type.py.
+"""
+
+import asyncio
+
+from vals.sdk.v2.util import get_ariadne_client
+
+
+async def generate_operator_type():
+    client = get_ariadne_client()
+    with open("operator_type_template", "r") as template:
+        template_text = template.read()
+
+    operator_list = await client.get_operators()
+    list_of_names = [f'"{op.name_in_doc}"' for op in operator_list.operators]
+
+    template_text = template_text.replace("<REPLACE>", ",\n\t".join(list_of_names))
+
+    with open("operator_type.py", "w") as f:
+
+        f.write(template_text)
+
+
+if __name__ == "__main__":
+    asyncio.run(generate_operator_type())
