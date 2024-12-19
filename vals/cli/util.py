9ab97ec9eb3f975757af91933595da1d33c0e8a1
Langston Nashold <lgnashold@gmail.com>
2024-12-18 19:15:09 -0800

    Remove old version of SDK

--- vals/cli/util.py
+++ vals/cli/util.py
@@ -1,7 +1,6 @@
 import sys
 
 import click
-from vals.sdk.suite import list_test_suites
 from vals.sdk.util import list_rag_suites
 
 
@@ -22,18 +21,6 @@ def prompt_user_for_rag_suite():
 def display_error_and_exit(error_msg: str):
     click.secho("ERROR: " + error_msg, fg="red")
     sys.exit(1)
-
-
-def prompt_user_for_suite():
-    suites = list_test_suites()
-    click.echo("Test Suites:")
-    click.echo("\n".join([f"{i}: {s['title']}" for i, s in enumerate(suites)]))
-
-    idx = click.prompt("Enter the number of the test suite", type=int)
-    while not 0 <= idx <= len(suites):
-        idx = click.prompt("Invalid choice. Retry", type=int)
-    suite_id = suites[idx]["id"]
-    return suite_id
 
 
 def display_table(
