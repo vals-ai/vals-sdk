9ad18afb4b1cdb64f91eb310a579be63d0cac0aa
Jarett <jarett@vals.ai>
2025-05-28 13:18:07 -0700

    Edit: refreshed graphine

--- vals/graphql_client/client.py
+++ vals/graphql_client/client.py
@@ -9,12 +9,10 @@ from .base_model import UNSET, UnsetType
 from .batch_add_question_answer_pairs import BatchAddQuestionAnswerPairs
 from .create_or_update_test_suite import CreateOrUpdateTestSuite
 from .create_question_answer_set import CreateQuestionAnswerSet
-from .create_rag_suite import CreateRagSuite
 from .delete_test_suite import DeleteTestSuite
 from .enums import RunStatus
 from .get_active_custom_operators import GetActiveCustomOperators
 from .get_operators import GetOperators
-from .get_rag_suites import GetRagSuites
 from .get_single_run_review import GetSingleRunReview
 from .get_test_data import GetTestData
 from .get_test_suite_data import GetTestSuiteData
@@ -756,28 +754,6 @@ class Client(AsyncBaseClient):
         data = self.get_data(response)
         return RemoveOldTests.model_validate(data)
 
-    async def create_rag_suite(
-        self, query: str, file_path: str, **kwargs: Any
-    ) -> CreateRagSuite:
-        _query = gql(
-            """
-            mutation createRagSuite($query: String!, $filePath: String!) {
-              updateRagSuite(ragSuiteId: "0", query: $query, filePath: $filePath) {
-                ragSuite {
-                  id
-                  query
-                }
-              }
-            }
-            """
-        )
-        variables: Dict[str, object] = {"query": query, "filePath": file_path}
-        response = await self.execute(
-            query=_query, operation_name="createRagSuite", variables=variables, **kwargs
-        )
-        data = self.get_data(response)
-        return CreateRagSuite.model_validate(data)
-
     async def rerun_tests(self, run_id: str, **kwargs: Any) -> RerunTests:
         query = gql(
             """
@@ -995,23 +971,3 @@ class Client(AsyncBaseClient):
         )
         data = self.get_data(response)
         return GetActiveCustomOperators.model_validate(data)
-
-    async def get_rag_suites(self, **kwargs: Any) -> GetRagSuites:
-        query = gql(
-            """
-            query getRagSuites {
-              ragSuites {
-                id
-                org
-                path
-                query
-              }
-            }
-            """
-        )
-        variables: Dict[str, object] = {}
-        response = await self.execute(
-            query=query, operation_name="getRagSuites", variables=variables, **kwargs
-        )
-        data = self.get_data(response)
-        return GetRagSuites.model_validate(data)
